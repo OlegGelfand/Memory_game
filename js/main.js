a58affc31f47d7961bd8a35deeda1bf50e36e9aa
OlegGelfand <57053418+OlegGelfand@users.noreply.github.com>
2020-02-18 00:28:28 -0500

    Update main.js
    portion of dom section

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 main.js
 
-const cards = ["queen", "queen","king", "king"];
+
+const cards = [{rank:"queen", suit:"hearts", cardImage:"images/queen-of-hearts.png"},{rank:"queen", suit:"diamonds", cardImage:"images/queen-of-diamonds.png"},{rank:"king", suit:"hearts", cardImage:"images/king-of-hearts.png"},{rank:"king", suit:"diamonds", cardImage:"images/king-of-diamonds.png"}];
+
 const cardsInPlay = [];
-const cardOne = 0;
-const cardTwo = 2;
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTwo');
+function checkForMatch(){if(cardsInPlay[1] === cardsInPlay[2]){alert("you found a match!")} else{alert("sorry try again")}};;
+function flipCard(cardId){checkForMatch(0);const cardOne = 0
+const cardTwo = 2; console.log("User flipped" + cards[cardId].rank)};
+cardsInPlay.push('cards[cardId].rank');
+flipCard(0);
+flipCard(2);
+console.log("images/queen-of-hearts");
+console.log("images/king-of-hearts");
+console.log("hearts");
+console.log("hearts");
 
-
-console.log("user flipped queen")
-
-console.log("user flipped king")
+function createBoard(){for (let i = 0; i < cards.length; i++){}}
 
 if (cardsInPlay.length === 2) {console.log(2)} 
- if(cardsInPlay[1] === cardsInPlay[2]){alert("you found a match!")} else{alert("sorry try again")};
+ 
